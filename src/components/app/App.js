8cb959967fa1523ae7ac41ff04b1afd3e65adaa8
Alejandro Gines <alejandro.gines.martinez@zalando.de>
2020-05-03 08:53:20 +0200

    feat: Implement routing manage through state in App component
    this commit contains: remove Header and Footer components from App,
    building simple footer using simply HTML5 semantic tag footer

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Questions from '../pages/questions';
+import QuestionDetails from '../pages/question-details';
+import QuestionForm from '../pages/question-form';
+import { QUESTIONS, QUESTION_DETAILS, QUESTION_FORM } from '../../utils/constants';
+
 import './App.css';
 
-import Header from '../header';
-import Footer from '../footer';
-
 function App() {
+  const [route, setRoute] = useState(QUESTIONS);
+  const [selectedQuestion, setSelectedQuestion] = useState(null);
+
+  function onChangeRoute(route) {
+    setRoute(route);
+  }
+  
+  const questionsProps = {
+    onChangeRoute,
+    setSelectedQuestion
+  }
+  const questionDetailsProps = {
+    onChangeRoute,
+    selectedQuestion
+  }
+
   return (
     <div className="app">
-      <Header />
-      <main>{'view'}</main>
-      <Footer />
+      { route === QUESTIONS && <Questions {...questionsProps}/> }
+      { route === QUESTION_DETAILS && <QuestionDetails {...questionDetailsProps}/> }
+      { route === QUESTION_FORM && <QuestionForm changeRoute={onChangeRoute}/> }
+      <footer>
+        {'Powered by'}
+        <a href=" http://docs.pollsapi.apiary.io/" alt="polls api">
+            {' Polls API'}
+        </a>
+    </footer>
 
     </div>
   );
