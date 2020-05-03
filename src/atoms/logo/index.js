ea42edb5cf1741c986513c5748be1f65979f7e7b
Alejandro Gines <alejandro.gines.martinez@zalando.de>
2020-05-03 14:33:11 +0200

    feat: Add styles to Logo, header with StyledComponents
    in this commit I add styledComponents in the same file as the component
    where they are consumed because the chance they are consumed by another
    component is at this point very low

--- src/atoms/logo/index.js
+++ src/atoms/logo/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
-import './index.css';
+import styled from 'styled-components';
 
-const Logo = () => (
-    <img src={logo} className="app-logo" alt="logo" />
+import logo from '../../assets/logo.svg';
+import { QUESTIONS } from '../../utils/constants';
+
+const StyledLogo = styled.img`
+    height: 100%;
+    pointer-events: none;
+`;
+
+const StyledTitle = styled.h1`
+    font-size: 4vmin;
+    font-weight: 900;
+    color: #E7F0EF;
+    letter-spacing: 0.1vmin;
+    font-family: Arial, Helvetica, sans-serif;
+    height: 100%;
+    display: inline-block;
+    margin-left: 20px;
+    vertical-align: middle;
+`;
+
+const StyledLogoWrapper = styled.div`
+    height: 10vmin; 
+    display: flex;
+    cursor: pointer;
+
+`;
+
+const Logo = ({onChangeRoute}) => (
+    <StyledLogoWrapper onClick={() => onChangeRoute(QUESTIONS)}>
+        <StyledLogo src={logo} className="app-logo" alt="logo" />
+        <StyledTitle>{'DEV POLL'}</StyledTitle>
+    </StyledLogoWrapper>
 );
 
 export default Logo;
